f950fcfc1e39f15adb38129d3e7c0742ac4670eb
Martin337 <gamarayburst@gmail.com>
2021-09-15 20:12:33 +0200

    Konacno

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //Only change code below this line
+
+        //Jos jedan primjer ovih komentara
+        
+        /*Ovi komentari
+        su doduse
+        malo duzi*/
+        
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
